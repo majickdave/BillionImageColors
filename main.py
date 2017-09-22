16bcf920eecf31cfbd52aef2c5e83bc33fd4fb19
majickdave <majickdave@gmail.com>
2017-09-21 19:35:47 -0700

    my first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from PIL import Image
+import cStringIO
+import urllib2
+from multiprocessing import Pool, TimeoutError
+import multiprocessing
+import csv
+
+
+""" Need to Implement output --> url;color;color;color """
+
+# SET URLS
+URLs = set()
+with open('data/urls.txt', 'r') as f:
+    # read data as lines of text
+    import pdb; pdb.set_trace()
+    url = f.readline()
+    if url not in URLs:
+        URLs.append()
+    URLs = list(set(URLs))
+
+
+# COLOR GRABBER - https://gist.github.com/zollinger/1722663 
+def getColors(infile, numcolors=3, resize=150):
+
+    image = Image.open(infile)                                              # Using PIL open image
+    image = image.resize((resize, resize))                                  # resize image to reduce pixel set
+    result = image.convert('P', palette=Image.ADAPTIVE, colors=numcolors)   # Convert to image pallete to get dominant colors
+    result.putalpha(0)                                                      # Set alpha to zero for RGB
+    colors = result.getcolors(resize*resize)                                # get colors in RGB of the resized image
+    colors = [y[:-1] for y in [x[-1] for x in colors]]                      # take RGB values
+    return colors
+
+# URL READER
+def readImage(url):
+    fl = None
+    try:
+        fl = cStringIO.StringIO(urllib2.urlopen(url).read())                # create image data from URL string
+    except:
+        print "Could not Download Image from Url"
+
+    finally:
+        print url, "completed"
+        return (url, getColors(fl))                                             # Return the formatted data string
+
+if __name__ == '__main__':
+    import timeit
+    time = timeit.default_timer()
+    numCPUs = multiprocessing.cpu_count()
+    try:
+        pool = Pool(processes=numCPUs*4)                                    # Pool processes using 4 times the CPUs for optimal performance time
+        data = pool.map(readImage, URLs)                                    
+    except TimeoutError:
+        print "Error, image downloading Timed Out"
+
+    with open("data/output.csv", 'wb') as csvFile:                          # Create CSV file
+        lineWriter = csv.writer(csvFile)
+        for line in data:
+            lineWriter.writerow((line[0], line[1][0], line[1][1], line[1][2]))
+
+    print("wall Time: ", timeit.default_timer() - time) 
+
+
+
+
+
+
+
+
+
