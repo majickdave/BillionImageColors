4bba2ec391ea1a56de384d7e2c24ef26be4f731a
majickdave <majickdave@gmail.com>
2017-09-21 22:02:29 -0700

    solved @ 0.35 s

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from PIL import Image
 import cStringIO
 import urllib2
-from multiprocessing import Pool, TimeoutError
-import multiprocessing
-import csv
 
+""" 
+Need to Implement output .csv --> url;color;color;color 
 
-""" Need to Implement output --> url;color;color;color """
-
-# SET URLS
-URLs = set()
-with open('data/urls.txt', 'r') as f:
-    # read data as lines of text
-    import pdb; pdb.set_trace()
-    url = f.readline()
-    if url not in URLs:
-        URLs.append()
-    URLs = list(set(URLs))
-
+Must handle large number of input
+"""
 
 # COLOR GRABBER - https://gist.github.com/zollinger/1722663 
 def getColors(infile, numcolors=3, resize=150):
@@ -31,7 +20,7 @@ def getColors(infile, numcolors=3, resize=150):
     return colors
 
 # URL READER
-def readImage(url):
+def downloadImage(url):
     fl = None
     try:
         fl = cStringIO.StringIO(urllib2.urlopen(url).read())                # create image data from URL string
@@ -39,25 +28,50 @@ def readImage(url):
         print "Could not Download Image from Url"
 
     finally:
-        print url, "completed"
-        return (url, getColors(fl))                                             # Return the formatted data string
+        # print url, "completed"
+        return getColors(fl)                                         # Return the formatted data string
 
+# MAIN APPLICATION
 if __name__ == '__main__':
+    import numpy as np
+    import csv
     import timeit
-    time = timeit.default_timer()
-    numCPUs = multiprocessing.cpu_count()
-    try:
-        pool = Pool(processes=numCPUs*4)                                    # Pool processes using 4 times the CPUs for optimal performance time
-        data = pool.map(readImage, URLs)                                    
-    except TimeoutError:
-        print "Error, image downloading Timed Out"
 
-    with open("data/output.csv", 'wb') as csvFile:                          # Create CSV file
-        lineWriter = csv.writer(csvFile)
-        for line in data:
-            lineWriter.writerow((line[0], line[1][0], line[1][1], line[1][2]))
+    # SET URLS
+    URLs = set()
+    # Processing times
+    times = []
 
-    print("wall Time: ", timeit.default_timer() - time) 
+    # Read Urls from textfile line by line
+    with open("data/urls.txt") as f:
+
+        # CREATE CSV
+        csvFile = open("data/output.csv", 'wb')
+        for url in f:
+            # time each url
+            time = timeit.default_timer()
+            # check if URL in set
+            hmap = url.partition("""//""")[2]       # reduce url string required to search for in set
+            if hmap not in URLs:
+                print hmap
+                # add unique url to set
+                URLs.add(hmap)
+                # download image from url and get 3 prevalent colors
+                data = downloadImage(url)
+                # create line writer object for csv file          
+                lineWriter = csv.writer(csvFile)
+                # write rows per spec
+                lineWriter.writerow((url, data[0], data[1], data[2]))
+                # track processing time
+                times.append((timeit.default_timer() - time))
+
+                print "."*len(url)
+
+        csvFile.close()
+        # when no more urls
+        print "Mean Processing Time per image: ", round(np.mean(times), 2), "seconds."
+        
+         
 
 
 
